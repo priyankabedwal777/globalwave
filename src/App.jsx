75cf2360145c32abc0a303601bae93f519d4074f
priyankabedwal777 <priyankaberwal3@gmail.com>
2025-11-13 19:26:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className=" flex text-5xl  align-bottom justify-center font-bold text-cyan-400 border-4 border-amber-600">
+        test
+      </div>
+    </>
+  );
+}
+
+export default App;
