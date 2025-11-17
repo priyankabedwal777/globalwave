2ff618853a458dd188688ef09e887b80512419fb
priyankabedwal777 <priyankaberwal3@gmail.com>
2025-11-17 08:45:20 +0530

    work on industries form

--- src/Components/Industries/index.jsx
+++ src/Components/Industries/index.jsx
@@ -4,8 +4,8 @@ import indimg2 from "../../assets/industries2.png";
 
 const Industries = () => {
   return (
-    <section className="py-16 px-4 bg-[#F4F6FF]">
-      <div className="max-w-6xl mx-auto text-center mb-12">
+    <section className="mt-16 py-20 px-20 bg-[#F4F6FF]">
+      <div className="mb-12">
         <h1 className="text-4xl font-bold text-[#EA372F] mb-4">
           Industries We Serve
         </h1>
@@ -14,48 +14,53 @@ const Industries = () => {
           challenges and unlock the full potential of your global teams.
         </p>
       </div>
-      <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-bold mb-4">Aviation</h2>
-          <p className="font-semibold mb-4">
-            Your High-Performing Global Center Engineered to Accelerate
-            Innovation in Aviation.
-          </p>
-          <p className="text-gray-600 mb-6">
-            The aviation industry moves at the speed of light—and so should your
-            operational capabilities. We partner with the world's leading
-            airlines, aerospace manufacturers, and aviation technology companies
-            to build, manage, and scale fully integrated Global Capability
-            Centers.
-          </p>
-          <div className="flex justify-between items-center">
-            <button className="bg-[#EA372F] text-white px-6 py-2 rounded hover:bg-red-600">
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="bg-white rounded-lg shadow-lg">
+          <div className="p-8">
+            <h2 className="text-2xl font-bold mb-4 text-[#EA372F]">Aviation</h2>
+            <p className="font-semibold mb-4">
+              Your High-Performing Global Center Engineered to Accelerate
+              Innovation in Aviation. loreem ipsum dolor sit amet, consectetur
+            </p>
+            <p className="text-gray-600 mb-6">
+              The aviation industry moves at the speed of light—and so should
+              your operational capabilities. We partner with the world's leading
+              airlines, aerospace manufacturers, and aviation technology
+              companies to build, manage, and scale fully integrated Global
+              Capability Centers.
+            </p>
+          </div>
+          <div className="flex  items-start">
+            <button className="bg-[#EA372F] text-white px-5 py-2 ml-8 rounded hover:bg-red-600">
               Know More
             </button>
-            <img src={indimg1} className="w-16 h-16" />
+            <img src={indimg1} className="" />
           </div>
         </div>
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-bold text-[#EA372F] mb-4">
-            Shipping & Logistics
-          </h2>
-          <p className="font-semibold mb-4">
-            Your High-Performing Global Center: The Strategic Backbone for
-            Modern Shipping & Logistics.
-          </p>
-          <p className="text-gray-600 mb-6">
-            The global supply chain demands precision, efficiency, and
-            resilience. We build in-house Global Capability Centers that serve
-            as the operational core for shipping lines, logistics providers, and
-            freight forwarders. Our solutions are designed to solve the most
-            complex industry challenges, including real-time fleet management,
-            supply chain optimization, and customs automation.
-          </p>
-          <div className="flex justify-between items-center">
-            <button className="bg-[#EA372F] text-white px-6 py-2 rounded hover:bg-red-600">
+        <div className="bg-white rounded-lg shadow-lg">
+          <div className="p-8">
+            <h2 className="text-2xl font-bold text-[#EA372F] mb-4">
+              Shipping & Logistics
+            </h2>
+            <p className="font-semibold mb-4">
+              Your High-Performing Global Center: The Strategic Backbone for
+              Modern Shipping & Logistics.
+            </p>
+            <p className="text-gray-600 mb-6">
+              The global supply chain demands precision, efficiency, and
+              resilience. We build in-house Global Capability Centers that serve
+              as the operational core for shipping lines, logistics providers,
+              and freight forwarders. Our solutions are designed to solve the
+              most complex industry challenges, including real-time fleet
+              management, supply chain optimization, and customs automation.
+            </p>
+          </div>
+          <div className="flex items-start">
+            <button className="bg-[#EA372F] text-white px-5 py-2 ml-8 rounded hover:bg-red-600">
               Know More
             </button>
-            <img src={indimg2} className="w-16 h-16" />
+            <img src={indimg2} className="" />
           </div>
         </div>
       </div>
